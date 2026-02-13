4ede0dcbc3e98c18320b1a1f92ff1e8be20f11dd
Pia-S <piasheth@outlook.com>
2026-02-12 17:20:00 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,4 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -6,14 +5,30 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Subsystems.ShooterSubsystem;
+import frc.robot.Subsystems.ShooterSubsystem.MotorSpeeds;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 
 public class RobotContainer {
-  public RobotContainer() {
+
+  ShooterSubsystem s_Shooter = ShooterSubsystem.getInstance();
+
+  CommandXboxController pjyController = new CommandXboxController(0);
+  
+  private static RobotContainer container;  
+  
+  private void configureBindings() {
+    //Review lambdas
+     pjyController.rightTrigger().onTrue(new InstantCommand(() -> s_Shooter.setMotorSpeed(MotorSpeeds.OUTTAKE)));
+     pjyController.rightTrigger().onFalse(new InstantCommand(() -> s_Shooter.setMotorSpeed(MotorSpeeds.HOLD)));
+  }
+   
+   public RobotContainer() {
     configureBindings();
   }
-
-  private void configureBindings() {}
-
+   
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
