56a6993c8abe954caeaa3d655810a36620e80ec4
Potato13954 <aadityadani12@gmail.com>
2026-02-12 18:20:24 -0800

    Changes to RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,3 +18,65 @@ public class RobotContainer {
     return Commands.print("No autonomous command configured");
   }
 }
+/*
+ * // Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Subsystems.IntakeSubsystem;
+
+
+
+
+public class RobotContainer {
+
+  public final IntakeSubsystem intakeSubsystem = IntakeSubsystem.getInstance();
+
+  private static RobotContainer container;
+
+  public static StartIntake startintake = new StartIntake.getInstance();
+  public static StopIntake stopintake = new StopIntake.getInstance();
+
+  public static RobotContainer getInstance(){
+
+    if (container == null){
+      container = new RobotContainer();
+    }
+    return container;
+
+  }
+
+  public final CommandXboxController driver = new CommandXboxController(0);
+  public final CommandXboxController operator = new CommandXboxController(1);
+
+  
+
+  private final Trigger driverLeftTrigger = driver.leftTrigger();
+
+  public CommandXboxController getDriverController(){
+    return driver;
+  }
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+    driverLeftTrigger.toggleOnTrue(() -> intakeSubsystem.setMotorSpeed(MotorSpeeds.SLOW()));
+
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
+
+ */
